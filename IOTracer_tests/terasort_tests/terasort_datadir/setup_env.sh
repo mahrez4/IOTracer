8a9ff753f04df61c1f71a1e568f7028d7a790a67
mahrez23 <jm_benbia@esi.dz>
2024-05-29 20:28:53 +0100

    terasort environment setup fixed

--- IOTracer_tests/terasort_tests/terasort_datadir/setup_env.sh
+++ IOTracer_tests/terasort_tests/terasort_datadir/setup_env.sh
@@ -1,9 +1,32 @@
-export HADOOP_HOME=/home/mhrz/pfe/tools/hadoop
-export HADOOP_CONF_DIR=/home/mhrz/pfe/tools/hadoop/etc/hadoop
-export HADOOP_MAPRED_HOME=/home/mhrz/pfe/tools/hadoop
-export HADOOP_COMMON_HOME=/home/mhrz/pfe/tools/hadoop
-export HADOOP_HDFS_HOME=/home/mhrz/pfe/tools/hadoop
-export YARN_HOME=/home/mhrz/pfe/tools/hadoop
+
+HADOOP_ARCHIVE="hadoop-3.3.6.tar.gz"
+HADOOP_DIR="hadoop"
+
+if [ -f "$HADOOP_ARCHIVE" ]; then
+    echo "Hadoop archive found: $HADOOP_ARCHIVE"
+    if [ ! -d "$HADOOP_DIR" ]; then
+	echo "Extracting Hadoop archive..."
+        tar -xzf "$HADOOP_ARCHIVE"
+        mv hadoop-3.3.6 $HADOOP_DIR
+    else
+        echo "Hadoop directory already exists: $HADOOP_DIR"
+    fi
+elif [ -d "$HADOOP_DIR" ]; then
+    echo "Hadoop directory found: $HADOOP_DIR"
+else
+    echo "Downloading hadoop"
+    wget  https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6-src.tar.gz 
+    echo "Extracting Hadoop archive..."
+    tar -xzf "$HADOOP_ARCHIVE"
+    mv hadoop-3.3.6 $HADOOP_ARCHIVE
+fi
+
+export HADOOP_HOME=$(pwd)/$HADOOP_DIR
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
 
 export JAVA_HOME=/usr/lib/jvm/java
-export PATH=$PATH:/home/mhrz/pfe/tools/hadoop/bin
+export PATH=$PATH:$HADOOP_HOME/bin
