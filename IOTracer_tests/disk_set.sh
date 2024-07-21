06800b4016685bacd6b58831b5ae3e2f9e3d76dc
mahrez23 <jm_benbia@esi.dz>
2024-07-22 00:52:11 +0100

    typo fixes

--- IOTracer_tests/disk_set.sh
+++ IOTracer_tests/disk_set.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -z  "$1"]; then
+if [  ! -z  "$1" ]; then
     if [[ "$1" == "sda" ]]; then
         traced_path='IOTRACER_PATH="/home/ensta/IOTracer/bcc_iotracer.py"'
         sed -i "s|IOTRACER_PATH=\"../../bcc_iotracer.py\"|$traced_path|" *_tests/run_benchmarks*
         sed -i "s/^disk=.*/disk=$1/" run_alltests.sh
         db_path=/media/SSDSM87-S2D4NYAG905047/mongodb
-        sed -i "s|dbPath: .*|dbPath: $db_path|" "$file"
+        sed -i "s|dbPath: .*|dbPath: $db_path|" /etc/mongod.conf
         systemctl restart mongod
     fi
     if [[ "$1" == "sdb" ]]; then
@@ -14,7 +14,7 @@ if [ ! -z  "$1"]; then
         sed -i "s|IOTRACER_PATH=\"../../bcc_iotracer.py\"|$traced_path|" *_tests/run_benchmarks*
         sed -i "s/^disk=.*/disk=$1/" run_alltests.sh
         db_path=/media/HDD/mongodb
-        sed -i "s|dbPath: .*|dbPath: $db_path|" "$file"
+        sed -i "s|dbPath: .*|dbPath: $db_path|" /etc/mongod.conf
         systemctl restart mongod
     fi
     if [[ "$1" == "sdc" ]]; then
@@ -22,7 +22,7 @@ if [ ! -z  "$1"]; then
         sed -i "s|IOTRACER_PATH=\"../../bcc_iotracer.py\"|$traced_path|" *_tests/run_benchmarks*
         sed -i "s/^disk=.*/disk=$1/" run_alltests.sh
         db_path=/media/SSD850-S251NXAGA20303P/mongodb
-        sed -i "s|dbPath: .*|dbPath: $db_path|" "$file"
+        sed -i "s|dbPath: .*|dbPath: $db_path|" /etc/mongod.conf
         systemctl restart mongod
     fi
     if [[ "$1" == "sdd" ]]; then
@@ -30,7 +30,7 @@ if [ ! -z  "$1"]; then
         sed -i "s|IOTRACER_PATH=\"../../bcc_iotracer.py\"|$traced_path|" *_tests/run_benchmarks*
         sed -i "s/^disk=.*/disk=$1/" run_alltests.sh
         db_path=/media/SSDSM87-S2D4NYAG905047/mongodb
-        sed -i "s|dbPath: .*|dbPath: $db_path|" "$file"
+        sed -i "s|dbPath: .*|dbPath: $db_path|" /etc/mongod.conf
         systemctl restart mongod
     fi
 fi
